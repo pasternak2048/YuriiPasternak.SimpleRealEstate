1cbfc2f4c7e826f18cf053acd2627222d1dae341
Yurii Pasternak <10pka.muscatel@gmail.com>
2024-05-24 15:45:27 +0300

    Added Identity.

---
+++ YuriiPasternak.SimpleRealEstate.Domain/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace YuriiPasternak.SimpleRealEstate.Domain.Identity
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ YuriiPasternak.SimpleRealEstate.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YuriiPasternak.SimpleRealEstate.Domain.Identity;
+using YuriiPasternak.SimpleRealEstate.Infrastructure.Context;
+
+namespace YuriiPasternak.SimpleRealEstate.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(connectionString));
+
+            services.AddIdentityCore<AppUser>()
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+        }
+    }
+}
